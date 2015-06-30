2d19a7d4cdb60bb8e0cb425b429d49d85e6efb96
note109 <note109@gmail.com>
2015-07-01 01:31:17 +0900

    add sample code

--- components/SlackViewer.react.js
+++ components/SlackViewer.react.js
@@ -19,3 +19,36 @@ var SlackViewer = React.createClass({
     );
   }
 });
+
+// var FilterableProductTable = React.createClass({
+//     getInitialState: function() {
+//         return {
+//             filterText: '',
+//             inStockOnly: false
+//         };
+//     },
+//
+//     handleUserInput: function(filterText, inStockOnly) {
+//         this.setState({
+//             filterText: filterText,
+//             inStockOnly: inStockOnly
+//         });
+//     },
+//
+//     render: function() {
+//         return (
+//             <div>
+//                 <SearchBar
+//                     filterText={this.state.filterText}
+//                     inStockOnly={this.state.inStockOnly}
+//                     onUserInput={this.handleUserInput}
+//                 />
+//                 <ProductTable
+//                     products={this.props.products}
+//                     filterText={this.state.filterText}
+//                     inStockOnly={this.state.inStockOnly}
+//                 />
+//             </div>
+//         );
+//     }
+// });
