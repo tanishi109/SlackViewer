8e5c578a4a372753200587b8ad5faddaafb32dd4
note109 <note109@gmail.com>
2015-07-08 00:46:36 +0900

    component化した

--- components/SlackViewer.react.js
+++ components/SlackViewer.react.js
@@ -1,69 +1,54 @@
-
-// https://github.com/inkenkun/react-native-todo/blob/master/App/components/TodoReact.react.js
-// module exports したらいけるやん！！！！
-
-
 var React = require('react-native');
 var token = "xoxp-2435567268-2435567274-6653820643-5ee449";
 
+var {
+  Text,
+  View,
+} = React;
+
 var SlackViewer = React.createClass({
-  getInitialState: function() {
+  getInitialState: function () {
+    return {
+      channels: "a",
+    };
+  },
+  componentDidMount: function() {
+    this.fetchData();
+  },
+  fetchData: function() {
     fetch('https://slack.com/api/channels.list?token='+token)
       .then((response) => response.text())
       .then((responseText) => {
-        console.log(responseText);
+        var values = JSON.parse(responseText);
+        var channelNames = [];
+        values.channels.forEach(function(channel) {
+          channelNames.push(channel.name);
+        });
+        this.setState({
+          channels: channelNames,
+          loaded: true,
+        });
       })
       .catch((error) => {
         console.warn(error);
-      });
-
-    return (
-      "hoge"
-    );
+      })
+      .done();
   },
 
   render: function() {
+    console.log("-----------")
+    console.log(this)
+    if (!this.state.channels) {
+      return null;
+    }
     return (
       <View>
         <Text>
-          This is very first app made by react!!
+          This is very first app made by react!! {this.state.channels}
         </Text>
       </View>
     );
   }
 });
 
-
-// var FilterableProductTable = React.createClass({
-//     getInitialState: function() {
-//         return {
-//             filterText: '',
-//             inStockOnly: false
-//         };
-//     },
-//
-//     handleUserInput: function(filterText, inStockOnly) {
-//         this.setState({
-//             filterText: filterText,
-//             inStockOnly: inStockOnly
-//         });
-//     },
-//
-//     render: function() {
-//         return (
-//             <div>
-//                 <SearchBar
-//                     filterText={this.state.filterText}
-//                     inStockOnly={this.state.inStockOnly}
-//                     onUserInput={this.handleUserInput}
-//                 />
-//                 <ProductTable
-//                     products={this.props.products}
-//                     filterText={this.state.filterText}
-//                     inStockOnly={this.state.inStockOnly}
-//                 />
-//             </div>
-//         );
-//     }
-// });
-
+module.exports = SlackViewer;
--- index.ios.js
+++ index.ios.js
@@ -5,84 +5,10 @@
 'use strict';
 
 var React = require('react-native');
-
-// var someComponent = require('./components/current');
-var token = "xoxp-2435567268-2435567274-6653820643-5ee449";
-
-var SlackViewer = React.createClass({
-  getInitialState: function () {
-    return {
-      channels: "a",
-    };
-  },
-  componentDidMount: function() {
-    this.fetchData();
-  },
-  fetchData: function() {
-    fetch('https://slack.com/api/channels.list?token='+token)
-      .then((response) => response.text())
-      .then((responseText) => {
-        var values = JSON.parse(responseText);
-        var channelNames = [];
-        values.channels.forEach(function(channel) {
-          channelNames.push(channel.name);
-        });
-        this.setState({
-          channels: channelNames,
-          loaded: true,
-        });
-      })
-      .catch((error) => {
-        console.warn(error);
-      })
-      .done();
-  },
-
-  render: function() {
-    console.log("-----------")
-    console.log(this)
-    if (!this.state.channels) {
-      return null;
-    }
-    return (
-      <View>
-        <Text>
-          This is very first app made by react!! {this.state.channels}
-        </Text>
-      </View>
-    );
-  }
-});
-// var ChannelsList = require('./components/ChannelsList');
-// var Channel = require('./components/Channel');
-
-// var SlackViewer = React.createClass({
-//   render: function() {
-// return (
-//   <View style={styles.container}>
-// <Text style={styles.welcome}>
-//   This is very first app made by react!!
-// </Text>
-// <Text style={styles.welcome}>
-//   Welcome to React Native!
-// </Text>
-// <Text style={styles.instructions}>
-//   To get started, edit index.ios.js
-// </Text>
-// <Text style={styles.instructions}>
-//   Press Cmd+R to reload,{'\n'}
-//   Cmd+D or shake for dev menu
-// </Text>
-//   </View>
-// );
-//   }
-// });
+var SlackViewer = require('./components/SlackViewer.react');
 
 var {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View,
 } = React;
 
 // var styles = StyleSheet.create({
